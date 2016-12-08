785367371410ea74cfc2b8ede3051c414398df3e
Marcelo McAndrew <Marcelo McAndrew>
2016-12-08 12:24:42 -0500

    added lots of comments, need to implement a function for adding phone number

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Allows user to automatically post on craigslist through entering information in 
 ##How to use##
 
 1. Make a copy of the `post.json` file in the posts file and name it whatever you want. e.g `nikes.json`
-2. Open the "nikes.json" in a text editor and change the arguments to your post
+2. Open the `nikes.json` in a text editor and change the arguments to your post
 	*You can add images in the `"img"` array by putting the file location in qoutes and using a comma to seperate different images
 3. Open the folder clpostbot folder
 4. Run node and type `node clpostbot.js`
--- clpostbot.js
+++ clpostbot.js
@@ -1,3 +1,6 @@
+//Created by: Marcelo McAndrew
+
+//require all the dependencies
 var jquery = require('jquery')
 var Nightmare = require('nightmare')
 var uploader = require('nightmare-upload')(Nightmare);
@@ -5,19 +8,22 @@ var prompt = require('prompt')
 var fs = require('fs')
 var path = require('path')
 
+//clear the local storage everytime nightmare runs
 var nightmare = Nightmare({
   webPreferences: {
     partition: 'nopersist'
   }
 })
 
+//url of craigslist
 var url="http://washingtondc.craigslist.org/"
 
+//require user to input password and post.json file
 var schema = {
   properties: {
     password: {
       required: true,
-      hidden: true
+      hidden: true //hide password when user types it
 
     },
     post_file: {
@@ -27,102 +33,77 @@ var schema = {
   }
 };
 
-
+//start the prompt
 prompt.start();
+//prompts can be accessed by result.password and result.post_file
 prompt.get(schema, function (err, result) {
 
-  // Get content from file
+  //get the post_file from the post folder in the root directory
   var contents = fs.readFileSync(path.resolve('posts')+'/'+result.post_file);
-  // Define to JSON type
+  //parse contents e.g post.img, post.title
   var post = JSON.parse(contents);
 
+  //start nightmare and set the viewport and go to craigslist url
   nightmare.viewport(2560,1440).goto(url)
-  //loads craigslist home page
 
-    .wait('#post')
-    //waits 1/2 of a second for the page to load
-    .click('#post')
-    //clicks 'post to classifieds' (#post)
-    .click('input[value=fso]')
-    //clicks the for sale by owner radio button
-    .click('.pickbutton')
-    //clicks the continue button
-    .wait(500)
-    //waits 1/2 second for the page to load
-    .click('label:nth-of-type('+post.category+')')
-    //type 18: clothing
-    //type 22: electronics
-    //clicks the electronics by owner radio button
+    //on the craigslist home page
+    .wait('#post')//wait for the post element to load
+    .click('#post')//click "post to classifieds"
 
-    .wait(500)
-    //waits 1/2 of a second for the page to load
+    //on the choose sale type page
+    .click('input[value=fso]')//click the "for sale by owner" radio button
+    .click('.pickbutton')//click continue
 
-    .click('label:nth-of-type(3)')
-    //clicks on location radio button
-    .click('.pickbutton')
-    //clicks continue
+    //on the select category page
+    .wait(500)//wait 500ms for page to load
+    .click('label:nth-of-type('+post.category+')')//select category from JSON
 
-    //now you're at the posting page
+    //on the location page
+    .wait(500)//wait 500ms for page to load
+    .click('label:nth-of-type(3)')//select Maryland
+    .click('.pickbutton')//hit continue
 
-    .wait(500)
-    //wait 1/2 of a second for the page to load
-    .click('a[href^="https"]')
-    //lets the user login
+    //on the post page
+    .wait(500)//wait 500ms for page to load
+    .click('a[href^="https"]')//click login
+    .wait(500)//wait 500ms for page to load
+    .type('#inputEmailHandle', post.email)//enter the email from the JSON
+    .type('#inputPassword', result.password)//enter the password from the prompt
+    .wait(500)//wait 500ms for page to load
+    .click('.accountform-btn')//hit login
 
-    .wait(500)
-    //wait 1/2 of a second for the page to load
-    .type('#inputEmailHandle', post.email)
-    .type('#inputPassword', result.password)
-    //fills in email and password from the prompt
-    .wait(500)
-    //wait 1/2 of a second for the page to load
-    .click('.accountform-btn')
+    //on the post page
+    .wait(1000)//wait 1 second for page to load
+    .insert('#PostingTitle', post.postTitle)//enters post title from JSON
+    .insert('#Ask',post.price)//enters post price from JSON
+    .insert('#PostingBody',post.postBody)//enters post body from JSON
+    .insert('#postal_code',post.postalCode)//enters postal code from JSON
+    .click('#wantamap')//uncheck "show on map"
+    .wait(250)//wait 250ms for page to load
+    .click('.bigbutton')//click publish
 
-    .wait(1000)
-    //wait 1/2 of a second for the page to load
-    .insert('#PostingTitle', post.postTitle)
-    .insert('#Ask',post.price)
-    .insert('#PostingBody',post.postBody)
-    .insert('#postal_code',post.postalCode)
-    .click('#wantamap')
-    //fills in the post contents*/
-    .wait(250)
-    //wait 1/4 of a second for the page to load
+    //image uploading page
+    .wait(1000)//wait 1 second for page to load
+    .click('#classic')//click the "classic image uploader" button
+    .wait(2000)
+    .end();//wait 2 seconds and end nightmare
 
-    .click('.bigbutton')
-    //proceed to image uploader
-    .wait(1000)
-
-    .click('#classic')
-
-    .wait(2000).end();
-    //wait 1/2 of a second for the page to load and end nightmare
-
+    //loops through and posts all images in the img array in the JSON
     for(var x=0;x<post.img.length;x++){
       nightmare.upload('input:nth-of-type(3)' , post.img[x]).wait(1000).end();
     }
-    //loops through the imgs
 
+    nightmare.wait(750)//start nightmare again and wait 750ms for page to load
 
-    nightmare.wait(750)//restart nightmare and wait
-
-    .click('button.done.bigbutton')//done w images
-    .wait(500)//wait 1/2 of a second
-
-    .click('.button')//publish the ad
-    .wait(2000)//wait 1/2 of a second
-
-
-
-    .screenshot('test.png')
-    //.click('button.pickbutton')
-
-
-
+    .click('button.done.bigbutton')//hit done with images
+    .wait(1000)//wait 1 seconds for page to load
+    .click('.button')//click the post button
+    .wait(2000)//wait 2 seconds for page to load
+    .screenshot('test.png')//take screenshot and use it as test.pngS
     .end()
 
     .then(function(result){
       //console.log(test);
     })
 
-    });
+});
